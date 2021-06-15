02847d25dc13a6e0f07c5ca5a1011658d65f7e2c
Philippe Douelle <philippe.douelle@consulting-for.edenred.com>
2021-06-15 11:48:23 +0200

    Initial commit

---
+++ pdouelle.RequestResponseLogginHttpClient/RequestResponseLoggingHttpClientHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pdouelle.RequestResponseLoggingHttpClient
+{
+    public class RequestResponseLoggingHttpClientHandler : DelegatingHandler
+    {
+        private readonly ILogger<RequestResponseLoggingHttpClientHandler> _logger;
+
+        public RequestResponseLoggingHttpClientHandler(ILogger<RequestResponseLoggingHttpClientHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            await LogRequest(request);
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            await LogResponse(response);
+
+            return response;
+        }
+
+        private async Task LogRequest(HttpRequestMessage request)
+        {
+            string content = null;
+
+            if (request.Content != null)
+                content = await request.Content.ReadAsStringAsync();
+
+            _logger.LogDebug($"Http Request Information: {request} Response Body: {content}");
+        }
+
+        private async Task LogResponse(HttpResponseMessage response)
+        {
+            string content = null;
+
+            if (response?.Content != null)
+                content = await response.Content.ReadAsStringAsync();
+
+            _logger.LogDebug($"Http Response Information: {response} Response Body: {content}");
+        }
+    }
+}
